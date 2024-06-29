393984c0673f4d4bda52404e6d5f6f1f086ba027
Patrick Oberholzer <patrickoberholzer08@gmail.com>
2024-06-29 15:54:14 -0400

    :)

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,24 @@ use anyhow::Result;
 const LETTERS: [char; 10] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 
 /// Status of cell on board
+#[derive(Clone, Copy, Debug)]
 enum Status {
     Empty,
     Missed,
-    Intact,
-    Hit,
-    Sunken,
+    Intact(u8),
+    Hit(u8),
+    Sunken(u8),
+}
+
+enum Orientation {
+    Horizontal,
+    Vertical,
 }
 
 struct Ship {
     length: u8,
-    start: (u8, u8)
+    start: (u8, u8),
+    orientation: Orientation,
 }
 
 // type Board = [[Status; 10]; 10];
@@ -41,9 +48,38 @@ fn main() {
 fn create_board(player_num: u8) -> Result<Board> {
     println!("Create a board!");
     let mut board_input = String::with_capacity(112);
-    let stdin = stdin::io::stdin();
+    let stdin = std::io::stdin();
     for _ in (0..10) {
         stdin.read_line(&mut board_input)?;
     }
 
+    let board: [[Status; 10]; 10] = board_input
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| match c {
+                    '-' => Status::Empty,
+                    n if n.is_numeric() => Status::Intact(n.to_digit(10).unwrap()),
+                    _ => panic!("Invalid character entered!"),
+                }).collect::<Vec<_>>()
+                .try_into()
+                .unwrap()
+        }).collect::<Vec<_>>()
+        .try_into()
+        .unwrap();
+
+    let mut ships: Vec<Ship> = Vec::with_capacity(5);
+    let mut seen_ships: Vec<u8> = Vec::with_capacity(5);
+    for (i, row) in board.iter().enumerate() {
+        for (j, cell) in row.iter().enumerate() {
+            let ship_num = match cell { 
+                Status::Hit(n) => n,
+                Status::Intact(n) => n,
+                Status::Sunken(n) => n,
+                _ = continue,
+            };
+            let to_right = board.get(i + 1).map(|r| r.get(j)).flatten()
+        }
+    }
+    todo!()
 }
